faa5edd6e8b26f2d7fa173f93cc2ba7a4286ba8b
mofunemmn <sinkamofunemu@gmail.com>
2021-01-17 23:22:44 +0900

    php coding start

---
+++ index.php
@@ -0,0 +1,70 @@
+
+<?php get_header(); ?>
+<main>
+    <section class="main-firstimage">
+        <div></div>
+        <img src="img/topimg.png" alt="BeeComeファーストビュー">
+    </section>
+    <section class="main-message">
+        <img class="main-message-PC" src="img/message.png" alt="">
+        <img class="main-message-sp" src="img/message-sp.png" alt="">
+    </section>
+    <section class="main-event" id="event">
+        <h2>EVENT</h2>
+        <img src="img/eventsample.jpg" alt="イベントの画像です">
+        <a href="#">イベント詳細ページへ</a>
+    </section>
+    <section class="main-activity" id="activity">
+        <h2>ACTIVITY</h2>
+        <div class="main-activity-script">
+            <h3>ポータブルスキルの習得と実践</h3>
+            <p>
+                ポータブルスキルとは、どのような仕事でも通用する「持ち運び可能な持ち運び可能な能力」です。<br>
+                BeeComeでは、講師企業から専門知識・技術を教わります。<br>
+                それをもとに地域交流イベントを開催し、実践を通して専門知識・技術をポータブルスキル<br>
+                として習得します。募集している班は専門ごとに4つです。
+            </p>
+        </div>
+        <div class="main-activity-articles">
+            <article class="main-activity-articles-item">
+                <h4>企画班</h4>
+                <img src="img/eventsample.jpg" alt="">
+                <ul>
+                    <li>０&rarr;１を作る経験をしたい</li>
+                    <li>企画・マネジメントに興味がある</li>
+                    <li>マネジメント力・実現力・判断力</li>
+                </ul>
+            </article>
+            <article class="main-activity-articles-item">
+                <h4>広報班</h4>
+                <img src="img/eventsample.jpg" alt="">
+                <ul>
+                    <li>デザインの知識を実践したい</li>
+                    <li>取材・編集・ライターに興味がある</li>
+                    <li>デザイン力・情報発信力</li>
+                </ul>
+            </article>
+            <article class="main-activity-articles-item">
+                <h4>IT班</h4>
+                <img src="img/eventsample.jpg" alt="">
+                <ul>
+                    <li>プログラミングの知識を実践したい</li>
+                    <li>HTML・CSSに触れたことがある</li>
+                    <li>プログラミングの基本と実践</li>
+                </ul>
+            </article>
+            <article class="main-activity-articles-item">
+                <h4>動画班</h4>
+                <img src="img/eventsample.jpg" alt="">
+                <ul>
+                    <li>動画制作を1から学びたい</li>
+                    <li>映像・演出・マスコミに興味がある</li>
+                    <li>動画企画・撮影・編集の基本と実践</li>
+                </ul>
+            </article>
+        </div>
+    </section>
+</main>
+<?php get_footer(); ?>
+
+    
