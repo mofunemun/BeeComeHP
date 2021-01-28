5e938896c0eb865a5a36f0f5e319a0260dd9a93a
mofunemmn <sinkamofunemu@gmail.com>
2021-01-28 13:59:00 +0900

    html/css/JS code finish

--- aside.js
+++ aside.js
@@ -13,3 +13,19 @@ $(".aside-sp-nav-item").on('click',function(){
     $(".header-sp-middle").toggleClass("is-open");
     $(".header-sp-bottom").toggleClass("is-open");
 });
+
+$(function() {
+    $('.slick').slick({
+        autoplay: false,
+        dots: true,
+        arrows: true,
+        prevArrow:'<img src="img/arrow-left.png" class="slide-arrow prev-arrow">',
+        nextArrow:'<img src="img/arrow-right.png" class="slide-arrow next-arrow">',
+    });
+});
+
+$(function(){
+    $("#slider").slick({
+      dotsClass: 'slide-dots'
+    });
+});
