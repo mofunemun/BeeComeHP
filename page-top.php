9df6c56dd504fdd50a8bd10f799f35652286dba1
mofunemmn <sinkamofunemu@gmail.com>
2021-01-29 13:36:33 +0900

    add new files

---
+++ page-top.php
@@ -0,0 +1,111 @@
+<?php get_header(); ?>
+<?php get_sidebar(); ?>
+    <main>
+        <section class="main-message">
+            <div class="main-message-first">
+                <img class="main-message-speech" src="<?php echo get_template_directory_uri(); ?>/img/speech-and-bee.png" alt="">
+                <img class="main-message-vivid" src="<?php echo get_template_directory_uri(); ?>/img/BeeCome-textrogo.png" alt="">
+                <img class="main-message-semivivid" src="<?php echo get_template_directory_uri(); ?>/img/BeeCome-textrogo.png" alt="">
+                <img class="main-message-vague" src="<?php echo get_template_directory_uri(); ?>/img/BeeCome-textrogo.png" alt="">
+            </div>
+            <article class="main-message-scripts">
+                <div class="main-message-scripts-headline" >
+                    <h2>”やりたい”が夢を運ぶ。</h2>
+                    <h2>さぁ、飛び立とう。</h2>
+                </div>
+                <div class="main-message-scripts-para">
+                    <p>飛び立とう。</p>
+                    <p>キミは青森のことをどれだけ知っているだろうか。</p>
+                    <p>
+                    知っているって、知識の話じゃないぜ？<br>
+                    “シゴト”の話をしているんだ。
+                    </p>
+                    <p>無知は最高に恐ろしい。</p>
+                    <p>
+                    知らないうちに、将来は狭まっている。<br>
+                    知るには、行動しかない。
+                    </p>
+                    <p>
+                        行動しないことを、<br>
+                        ウイルスの性には出来ないだろう。
+                    </p>
+                    <p>
+
+                        どうだい、ひとつ一緒に挑戦しないか。<br>
+                        方法なら、ここにある。
+                    </p>
+                </div>
+            </article>
+            </div>
+        </section>
+        <section class="main-event contents-inner" id="event">
+            <h2>EVENT</h2>
+            <ul class="slick">
+                <li class="slick-items"><img src="<?php echo get_template_directory_uri(); ?>/img/slide-img.png" alt="イベントの画像です"></li>
+                <li class="slick-items"><img src="<?php echo get_template_directory_uri(); ?>/img/kaikousya-air.png" alt="イベントの画像です"></li>
+                <li class="slick-items"><img src="<?php echo get_template_directory_uri(); ?>/img/slide-img.png" alt="イベントの画像です"></li>
+            </ul>
+            <div>
+                <a href="<?php get_template_directory_uri(); ?>/event-post.jpg">イベント詳細ページへ</a>
+            </div>
+        </section>
+        <section class="main-whats">
+            <h2>What's BeeCome?</h2>
+            <img class="main-whats-logoimg" src="<?php echo get_template_directory_uri(); ?>/img/BeeCome-logo.png" alt="ロゴの蜂の画像です">
+            <img class="main-whats-lectureimg" src="<?php echo get_template_directory_uri(); ?>/img/lecture.png" alt="活動の様子です">
+            <div class="main-whats-scripts">
+                <p>
+                    　”BeeCome”は学生が自分自身のやりたいことを実現させる団体です。<br>
+                    　BeeComeの学生は、講座で学習し、得た知識・技術を用いてイベントの企画運営を行います。<br>
+                    　企画班・ 広報班・ IT班・ 動画班に分かれます(詳細はACTIVITYにてご覧下さい)。
+                    講座では班ごとに専門性をもった企業を講師としてお呼びします。
+                </p>
+            </div>
+        </section>
+        <section class="main-activity" id="activity">
+            <h2>ACTIVITY</h2>
+            <div class="main-activity-script">
+                <h3>ポータブルスキルの習得と実践</h3>
+                <p>
+                    ポータブルスキルとは、どのような仕事でも通用する「持ち運び可能な持ち運び可能な能力」です。<br>
+                    BeeComeでは、講師企業から専門知識・技術を教わります。<br>
+                    それをもとに地域交流イベントを開催し、実践を通して専門知識・技術をポータブルスキルとして習得します。
+                </p>
+            </div>
+            <div class="main-activity-articles">
+                <article class="main-activity-articles-item">
+                    <h4>企画班</h4>
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/project-students.png" alt="">
+                    <ul>
+                        <li>０&rarr;１を作る経験</li>
+                        <li>企画・マネジメント</li>
+                    </ul>
+                </article>
+                <article class="main-activity-articles-item">
+                    <h4>広報班</h4>
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/project-students.png" alt="">
+                    <ul>
+                        <li>デザインの実践</li>
+                        <li>取材・編集・ライティング</li>
+                    </ul>
+                </article>
+                <article class="main-activity-articles-item">
+                    <h4>IT班</h4>
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/project-students.png" alt="">
+                    <ul>
+                        <li>Webデザインの実践をしたい</li>
+                        <li>プログラミングの実践</li>
+                    </ul>
+                </article>
+                <article class="main-activity-articles-item">
+                    <h4>動画班</h4>
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/project-students.png" alt="">
+                    <ul>
+                        <li>動画制作を1から学ぶ</li>
+                        <li>動画企画・撮影・編集の基本と実践</li>
+                    </ul>
+                </article>
+            </div>
+        </section>
+    </main>
+<?php get_footer(); ?>
