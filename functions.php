bdee65ed80bc73cb7a99cb87c628c8436705cf47
mofunemmn <sinkamofunemu@gmail.com>
2021-02-06 21:54:55 +0900

    func fixed

--- functions.php
+++ functions.php
@@ -1,10 +1,11 @@
 <?php
 function beecome_style() {
-        wp_enqueue_style( 'normalize', get_theme_file_uri() . '/css/reset.css', array(), '4.5.0' );
-        wp_enqueue_style( 'slick', get_theme_file_uri() . '/css/slick.css', array(), '4.5.0' );
-        wp_enqueue_style( 'beecome', get_theme_file_uri() . '/css/front.css', array(), '1.0.0' );
-        wp_enqueue_style( 'style', get_theme_file_uri() . '/style.css', array(), '1.0.0' );
-        wp_enqueue_script( 'aside', get_theme_file_uri() .'/aside.js', array(), '1.0.0', true);
-        wp_enqueue_script( 'slick', get_theme_file_uri() .'/slick.min.js', array(), '1.0.0', true); 
+			wp_enqueue_style( 'normalize', get_theme_file_uri() . '/css/reset.css', array(), '4.5.0' );
+			wp_enqueue_style( 'slick', get_theme_file_uri() . '/css/slick.css', array(), '4.5.0' );
+			wp_enqueue_style( 'beecome', get_theme_file_uri() . '/css/front.css', array(), '1.0.0' );
+			wp_enqueue_style( 'style', get_theme_file_uri() . '/style.css', array(), '1.0.0' );
+			wp_enqueue_script( 'Jquery-script', plugin_dir_url( __FILE__ ) . '/js/my-script.js', array( 'jquery' ), '1.0.0', true  );
+			wp_enqueue_script( 'aside', get_theme_file_uri() .'/aside.js', array('slick'), '1.0.0', true);
+			wp_enqueue_script( 'slick', get_theme_file_uri() .'/slick.min.js', array(), '1.0.0', true); 
     }
 add_action( 'wp_enqueue_scripts', 'beecome_style' );
