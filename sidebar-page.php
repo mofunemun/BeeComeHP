25874be16b04417a3afda285bba3c7c55f440472
mofunemmn <sinkamofunemu@gmail.com>
2021-02-07 01:21:48 +0900

    ｄさ

--- sidebar-page.php
+++ sidebar-page.php
@@ -1,18 +1,18 @@
 <aside class="aside-sp">
         <nav>
-            <div class="aside-sp-nav-item"><a href="<?php echo get_permalink(); ?>/#event">MOVIE</a></div>
-            <div class="aside-sp-nav-item"><a href="<?php echo get_permalink(); ?>/#activity">イベント概要</a></div>
-            <div class="aside-sp-nav-item"><a href="<?php echo get_permalink(); ?>/#contact">出店者情報</a></div>
-            <div class="aside-sp-nav-item"><a href="<?php echo get_permalink(); ?>/#contact">アクセス</a></div>
-            <div class="aside-sp-nav-item"><a href="<?php echo get_permalink(); ?>/#contact">注意事項</a></div>
+            <div class="aside-sp-nav-item"><a href="#event">MOVIE</a></div>
+            <div class="aside-sp-nav-item"><a href="#activity">イベント概要</a></div>
+            <div class="aside-sp-nav-item"><a href="#contact">出店者情報</a></div>
+            <div class="aside-sp-nav-item"><a href="#contact">アクセス</a></div>
+            <div class="aside-sp-nav-item"><a href="#contact">注意事項</a></div>
         </nav>
       </aside>
         <div class="header-nav">
           <img class="header-logo" src="<?php echo get_template_directory_uri(); ?>/img/BeeCome-logo.png" alt="BeeComeのロゴ">
           <img class="header-titlelogo" src="<?php echo get_template_directory_uri(); ?>/img/BeeCome-textrogo.png" alt="BeeComeのタイトルロゴ">
           <nav>
-              <a href="<?php echo get_permalink(); ?>/#first-block">HOME</a>
-              <a href="<?php echo get_permalink(); ?>/#second-block">EVENT</a>
+              <a href="#first-block">HOME</a>
+              <a href="#second-block">EVENT</a>
           </nav>
         </div>
         <button class="header-sp">
