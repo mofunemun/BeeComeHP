d7606ea1b8e6bd119e53d29397657aa4ef91cdfd
mofunemmn <sinkamofunemu@gmail.com>
2021-02-06 23:39:56 +0900

    echo add

--- sidebar-page.php
+++ sidebar-page.php
@@ -1,18 +1,18 @@
 <aside class="aside-sp">
         <nav>
-            <div class="aside-sp-nav-item"><a href="<?php get_template_directory_uri(); ?>/#event">MOVIE</a></div>
-            <div class="aside-sp-nav-item"><a href="<?php get_template_directory_uri(); ?>/#activity">イベント概要</a></div>
-            <div class="aside-sp-nav-item"><a href="<?php get_template_directory_uri(); ?>/#contact">出店者情報</a></div>
-            <div class="aside-sp-nav-item"><a href="<?php get_template_directory_uri(); ?>/#contact">アクセス</a></div>
-            <div class="aside-sp-nav-item"><a href="<?php get_template_directory_uri(); ?>/#contact">注意事項</a></div>
+            <div class="aside-sp-nav-item"><a href="<?php echo get_template_directory_uri(); ?>/#event">MOVIE</a></div>
+            <div class="aside-sp-nav-item"><a href="<?php echo get_template_directory_uri(); ?>/#activity">イベント概要</a></div>
+            <div class="aside-sp-nav-item"><a href="<?php echo get_template_directory_uri(); ?>/#contact">出店者情報</a></div>
+            <div class="aside-sp-nav-item"><a href="<?php echo get_template_directory_uri(); ?>/#contact">アクセス</a></div>
+            <div class="aside-sp-nav-item"><a href="<?php echo get_template_directory_uri(); ?>/#contact">注意事項</a></div>
         </nav>
       </aside>
         <div class="header-nav">
-          <img class="header-logo" src="<?php get_template_directory_uri(); ?>/img/BeeCome-logo.png" alt="BeeComeのロゴ">
-          <img class="header-titlelogo" src="<?php get_template_directory_uri(); ?>/img/BeeCome-textrogo.png" alt="BeeComeのタイトルロゴ">
+          <img class="header-logo" src="<?php echo get_template_directory_uri(); ?>/img/BeeCome-logo.png" alt="BeeComeのロゴ">
+          <img class="header-titlelogo" src="<?php echo get_template_directory_uri(); ?>/img/BeeCome-textrogo.png" alt="BeeComeのタイトルロゴ">
           <nav>
-              <a href="<?php get_template_directory_uri(); ?>/#first-block">HOME</a>
-              <a href="<?php get_template_directory_uri(); ?>/#second-block">EVENT</a>
+              <a href="<?php echo get_template_directory_uri(); ?>/#first-block">HOME</a>
+              <a href="<?php echo get_template_directory_uri(); ?>/#second-block">EVENT</a>
           </nav>
         </div>
         <button class="header-sp">
--- single.php
+++ single.php
@@ -1,20 +1,20 @@
 <?php get_header('page'); ?>
 <?php get_sidebar('page'); ?>
     <section id="first-block">
-      <image src="<?php get_template_directory_uri(); ?>/img/first-back-s.jpg" class="first-back-s"></image>
-      <image src="<?php get_template_directory_uri(); ?>/img/first-back-pc.jpg" class="first-back-pc"></image>
+      <image src="<?php echo get_template_directory_uri(); ?>/img/first-back-s.jpg" class="first-back-s"></image>
+      <image src="<?php echo get_template_directory_uri(); ?>/img/first-back-pc.jpg" class="first-back-pc"></image>
     </section>
     <section id="second-block">
         <div class="movie-container">
         　<h2>MOVIE</h2>
-          <image src="<?php get_template_directory_uri(); ?>/img/YouTube.jpg" class="YouTube-image"></image>
+          <image src="<?php echo get_template_directory_uri(); ?>/img/YouTube.jpg" class="YouTube-image"></image>
           <p class="info-text">
             <u>近日公開･･･！</u>
           </p>
         </div>
     </section>
     <section id="third-block">
-      <image src="<?php get_template_directory_uri(); ?>/img/event.png" class="event"></image>
+      <image src="<?php echo get_template_directory_uri(); ?>/img/event.png" class="event"></image>
     </section>
     <section id="fourth-block">
       <div class="shop-info">
@@ -25,37 +25,37 @@
                   <div class="card-index">
                     <article class="shop-card">
                       <h5>antique&cafe ROTTO</h5> 
-                      <img class="card-img" src="<?php get_template_directory_uri(); ?>/img/rotto.png" alt="">
+                      <img class="card-img" src="<?php echo get_template_directory_uri(); ?>/img/rotto.png" alt="">
                       <p class="card-text">弘前市にあるアンティーク家具2階にある週末限定の隠れ家的カフェ「ロット堂」。1階で集めたアンティーク家具などを使い、テーブルや椅子にもこだわっています。店内では古本の展示販売を行われています。</p>
                       <p class="card-address">〒036－8001 <br>弘前市代官町111-2　　0172-55-9174</p>
                     </article>
                     <article class="shop-card">
                       <h5>浪漫須貯古齢糖</h5> 
-                      <img class="card-img" src="<?php get_template_directory_uri(); ?>/img/romance.png" alt="">
+                      <img class="card-img" src="<?php echo get_template_directory_uri(); ?>/img/romance.png" alt="">
                       <p class="card-text">世界各国から厳選した高品質のカカオのみを選別、自家焙煎されている「浪漫須貯古齢糖」。カカオの魅力や可能性を最大限に活かし、ワンランク上のクラフトチョコレート作作られています。</p>
                       <p class="card-address">〒036－8332 <br>弘前市亀甲町5　　 0172-88-9015</p>
                     </article>
                     <article class="shop-card">
                       <h5>ジョージのおやつ</h5> 
-                      <img class="card-img" src="<?php get_template_directory_uri(); ?>/img/George.png" alt="">
+                      <img class="card-img" src="<?php echo get_template_directory_uri(); ?>/img/George.png" alt="">
                       <p class="card-text">カフェ兼スイーツ・ケーキショップの「ジョージのおやつ」。お菓子教室やイベント出店なども行っており、楽しい時も、苦しい時も、いつもあなたのそばに寄り添える、そんなおやつを提供されています。</p>
                       <p class="card-address">〒036－8102 <br>弘前市子比内5-1-1　　0172-28-1050</p>
                     </article>
                     <article class="shop-card">
                       <h5>コトリcafe</h5> 
-                      <img class="card-img" src="<?php get_template_directory_uri(); ?>/img/kotoricafe.png" alt="">
+                      <img class="card-img" src="<?php echo get_template_directory_uri(); ?>/img/kotoricafe.png" alt="">
                       <p class="card-text">カフェ兼スイーツ・ケーキショップの「ジョージのおやつ」。お菓子教室やイベント出店なども行っており、楽しい時も、苦しい時も、いつもあなたのそばに寄り添える、そんなおやつを提供されています。</p>
                       <p class="card-address">〒036－8035 <br>弘前市百石町3-2　　0172-88-8504</p>
                     </article>
                     <article class="shop-card">
                       <h5>カフェ デュボワ</h5> 
-                      <img class="card-img" src="<?php get_template_directory_uri(); ?>/img/duboa.png" alt="">
+                      <img class="card-img" src="<?php echo get_template_directory_uri(); ?>/img/duboa.png" alt="">
                       <p class="card-text">カフェ兼スイーツ・ケーキショップの「ジョージのおやつ」。お菓子教室やイベント出店なども行っており、楽しい時も、苦しい時も、いつもあなたのそばに寄り添える、そんなおやつを提供されています。</p>
                       <p class="card-address">〒036－8035 <br>弘前市百石町3-2　　0172-88-8504</p>
                     </article>
                     <article class="shop-card">
                       <h5>二階堂</h5> 
-                      <img class="card-img" src="<?php get_template_directory_uri(); ?>/img/nikaido.png" alt="">
+                      <img class="card-img" src="<?php echo get_template_directory_uri(); ?>/img/nikaido.png" alt="">
                       <p class="card-text">1946年創業、青森の老舗和菓子店「二階堂」。身体に優しく美味しいお菓子作りにこだわり何度も試作した末にたどりついた至高の逸品”あおもり藍フィナンシェ”を発売します。</p>
                       <p class="card-address">〒036－8035 <br>弘前市百石町3-2　　0172-88-8504</p>
                     </article>
@@ -64,49 +64,49 @@
                 <input type="checkbox" id="label2" class="cssacc" />
                   <div class="craft-index">
                     <div class="craft-card">
-                      <img class="img" src="<?php get_template_directory_uri(); ?>/img/kogin.jpg" alt="">
+                      <img class="img" src="<?php echo get_template_directory_uri(); ?>/img/kogin.jpg" alt="">
                       <p class="craft-info">弘前こぎん研究所<br>
                       <u class="craft-address">〒000-0000<br>
                                             弘前市御幸町00　0000-00-0000</u>
                       </p>
                     </div>
                     <div class="craft-card">
-                      <img class="img" src="<?php get_template_directory_uri(); ?>/img/kogin.jpg" alt="">
+                      <img class="img" src="<?php echo get_template_directory_uri(); ?>/img/kogin.jpg" alt="">
                       <p class="craft-info">弘前こぎん研究所<br>
                       <u class="craft-address">〒000-0000<br>
                                             弘前市御幸町00　0000-00-0000</u>
                       </p>
                     </div>
                     <div class="craft-card">
-                      <img class="img" src="<?php get_template_directory_uri(); ?>/img/kogin.jpg" alt="">
+                      <img class="img" src="<?php echo get_template_directory_uri(); ?>/img/kogin.jpg" alt="">
                       <p class="craft-info">弘前こぎん研究所<br>
                       <u class="craft-address">〒000-0000<br>
                                             弘前市御幸町00　0000-00-0000</u>
                       </p>
                     </div>
                     <div class="craft-card">
-                      <img class="img" src="<?php get_template_directory_uri(); ?>/img/kogin.jpg" alt="">
+                      <img class="img" src="<?php echo get_template_directory_uri(); ?>/img/kogin.jpg" alt="">
                       <p class="craft-info">弘前こぎん研究所<br>
                       <u class="craft-address">〒000-0000<br>
                                             弘前市御幸町00　0000-00-0000</u>
                       </p>
                     </div>
                     <div class="craft-card">
-                      <img class="img" src="<?php get_template_directory_uri(); ?>/img/kogin.jpg" alt="">
+                      <img class="img" src="<?php echo get_template_directory_uri(); ?>/img/kogin.jpg" alt="">
                       <p class="craft-info">弘前こぎん研究所<br>
                       <u class="craft-address">〒000-0000<br>
                                             弘前市御幸町00　0000-00-0000</u>
                       </p>
                     </div>
                     <div class="craft-card">
-                      <img class="img" src="<?php get_template_directory_uri(); ?>/img/kogin.jpg" alt="">
+                      <img class="img" src="<?php echo get_template_directory_uri(); ?>/img/kogin.jpg" alt="">
                       <p class="craft-info">弘前こぎん研究所<br>
                       <u class="craft-address">〒000-0000<br>
                                             弘前市御幸町00　0000-00-0000</u>
                       </p>
                     </div>
                     <div class="craft-card">
-                      <img class="img" src="<?php get_template_directory_uri(); ?>/img/kogin.jpg" alt="">
+                      <img class="img" src="<?php echo get_template_directory_uri(); ?>/img/kogin.jpg" alt="">
                       <p class="craft-info">弘前こぎん研究所<br>
                       <u class="craft-address">〒000-0000<br>
                                             弘前市御幸町00　0000-00-0000</u>
