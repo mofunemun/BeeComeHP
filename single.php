ddaec1c04e4f7c6646d26c53b059c930f684c8ef
mofunemmn <sinkamofunemu@gmail.com>
2021-02-07 22:32:58 +0900

    fixed final

--- single.php
+++ single.php
@@ -122,7 +122,7 @@
         <div class="map-container">
           <h4>アクセス</h4>
            <div class="map-container-main">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3029.768834848145!2d140.4771725!3d40.5908577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5f9b01bb6cca7535%3A0x5831a80a8d5ebb1a!2z5pen5byY5YmN5YGV6KGM56S-!5e0!3m2!1sja!2sjp!4v1612606268840!5m2!1sja!2sjp" width="300" height="200" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3029.768834848145!2d140.4771725!3d40.5908577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5f9b01bb6cca7535%3A0x5831a80a8d5ebb1a!2z5pen5byY5YmN5YGV6KGM56S-!5e0!3m2!1sja!2sjp!4v1612606268840!5m2!1sja!2sjp" width="100%" height="200" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
           　<p>
             <br>
             ――――――――――――ーーー <br>
