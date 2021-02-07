e60499abb63b0fb29187f0129e4142f589bce201
mofunemmn <sinkamofunemu@gmail.com>
2021-02-07 22:47:37 +0900

    all finish

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
             <img class="footer-QR" src="<?php echo get_template_directory_uri(); ?>/img/QRcode.png" alt="BeeComeインスタのQRコード">
         </div>
         <p class="footer-copyrights">学生団体　BeeCome<br></p>
-        <p class="footer-subcopyrights">学生団体　＠ぼくらしNext&times;HeroJect&times;青森県&times;NPO法人SEEDS SEEDS NETWORK</p>
+        <p class="footer-subcopyrights">学生団体　＠ぼくらしNext&times;HeroJect&times;青森県&times;NPO法人SEEDS NETWORK</p>
     </footer>
     <script src="./aside.js"></script>
     <script src="./slick.min.js"></script>
