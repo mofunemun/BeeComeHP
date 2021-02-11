8a070c8e714c83914dbd30fd5fd625ec77e9d442
mofunemmn <sinkamofunemu@gmail.com>
2021-02-11 19:58:13 +0900

    page title edit

--- header-page.php
+++ header-page.php
@@ -3,7 +3,7 @@
  <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>偕行社イベントページ</title>
+  <title>『ひとたび、ひろさき～おいしくてあたたかい社交場～』　学生団体BeeCome 偕行社イベントページ</title>
   <link rel="stylesheet" href="css/reset.css">
   <link rel="stylesheet" href="css/front.css">
   <link rel="stylesheet" type="text/css" href="css/kaikousya.css">
