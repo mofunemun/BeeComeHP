a529205e9b79a2e8600ed4be705e0a317f5d6e23
mofunemmn <sinkamofunemu@gmail.com>
2021-02-22 16:15:36 +0900

    footer change

--- footer-page.php
+++ footer-page.php
@@ -1,8 +1,8 @@
     <footer>  
       <div>
-        <img class="footer-logo" src="<?php echo get_template_directory_uri(); ?>/img/BeeCome-logo.png" alt="BeeComeのロゴ">
-        <img class="footer-titlelogo" src="<?php echo get_template_directory_uri(); ?>/img/BeeCome-textrogo.png" alt="BeeComeのタイトルロゴ">
-        <img class="footer-QR" src="<?php echo get_template_directory_uri(); ?>/img/QRcode.png" alt="BeeComeインスタのQRコード">
+          <a href="#"><img class="footer-logo" src="<?php echo get_template_directory_uri(); ?>/img/BeeCome-logo.png" alt="BeeComeのロゴ"></a>
+          <img class="footer-titlelogo" src="<?php echo get_template_directory_uri(); ?>/img/BeeCome-textrogo.png" alt="BeeComeのタイトルロゴ">
+          <a href="https://www.instagram.com/beecome_hirosaki/?hl=ja"><img class="footer-QR" src="<?php echo get_template_directory_uri(); ?>/img/QRcode.png" alt="BeeComeインスタのQRコード"></a>
     </div>
     <p class="footer-copyrights">学生団体　BeeCome<br></p>
     <p class="footer-subcopyrights">学生団体　＠ぼくらしNext&times;HeroJect&times;青森県&times;NPO法人SEEDS NETWORK</p>
